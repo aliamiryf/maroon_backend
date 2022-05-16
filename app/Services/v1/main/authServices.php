e6c15521fecae7fb95f594826b9532a27dfd3e47
aliamirycoder <aliamiryf1381@gmail.com>
2022-05-16 16:55:02 +0430

    working on register services

--- app/Services/v1/main/authServices.php
+++ app/Services/v1/main/authServices.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\v1\main;
 
+use App\Models\v1\User;
 use App\Services\BaseServices;
 
 class authServices extends BaseServices
@@ -9,7 +10,12 @@ class authServices extends BaseServices
     public function register($request)
     {
         $data = $this->validatorData($request->all(),[
-            'username'=>'required|unique:users'
+            'username'=>'required|unique:users',
+            'email'=>'required|unique:users|email',
+            'password'=>'required|min:6'
+        ]);
+        $user = User::create([
+            'username'=>$data['data']['username']
         ]);
         return $data;
     }
